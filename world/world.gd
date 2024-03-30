3a7527630e0c52aba8c360de048efddfc71b3c45
Youssef Wagih <youssefwagih212@gmail.com>
2024-03-30 23:11:39 +0200

    Fixed Climbing

--- world/world.gd
+++ world/world.gd
@@ -6,13 +6,4 @@ func _process(delta):
 		get_tree().change_scene_to_file("res://wasted_menu/wasted_menu.tscn")
 
 
-func _on_ladder_body_entered(body):
-	if body.name == "Player":
-		print("entered area")
-		body.is_climbing = true
 
-
-func _on_ladder_body_exited(body):
-	if body.name == "Player":
-		print("exited area")
-		body.is_climbing = false
